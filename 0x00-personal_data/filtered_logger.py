1a97c3fa4c3002c3dcf218868fb062fa63746414
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-09-13 11:28:36 +0000

    match password and bd

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Obfuscate log message
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator) -> str:
+    """
+    Obfuscate log message
+    """
+    for field in fields:
+        message = re.sub(field + '.*?' + separator, redaction, message)
+    return message
