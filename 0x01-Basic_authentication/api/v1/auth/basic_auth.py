7b0ace6690ca6d274d83eadfc3ee95a8d8e5018b
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-09-17 21:51:53 +0000

    extract base64 header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,13 @@ class BasicAuth(Auth):
     """
     BasicAuth Class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extract base64 authorization header
+        """
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if authorization_header.split()[0] != 'Basic':
+            return None
+        return authorization_header.split()[1]
