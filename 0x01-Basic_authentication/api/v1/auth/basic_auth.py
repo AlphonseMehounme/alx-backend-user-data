f35fc5805754296ada6674ff35b9a9b61b5bd0c0
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-09-18 11:02:57 +0000

    get user with details

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Basic Auth Module
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -46,3 +48,20 @@ class BasicAuth(Auth):
             return (None, None)
         credentials = decoded.split(':')
         return (credentials[0], credentials[1])
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """
+        Return user object based on credentials
+        """
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        users = User.search({'email': user_email})
+        if len(users) == 0:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
